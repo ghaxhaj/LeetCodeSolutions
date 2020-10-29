955a2f826708461ab49d7812961df641a28a16cb
Granit <ghaxhaj@fordham.edu>
2020-10-29 11:41:44 -0400

    Word Break Solved

--- solutions/WordBreak.js
+++ solutions/WordBreak.js
@@ -17,10 +17,26 @@
 // Input: s = "catsandog", wordDict = ["cats", "dog", "sand", "and", "cat"]
 // Output: false
 
-let wordBreak = (s, wordDict) => {
-    let array = s.split('')
-    return array
-}
+const wordBreak = (s, wordDict) => {
+    if(!wordDict) return false;
+      
+  //Create a DP table of len(s) elements, and set true when if mark index i when s(i) is a word that can be formed from wordDict
+    let dp = new Array(s.length + 1);
+    dp[0] = true; //word of length 0 is always true;
+    let matches=[];
+    for(let i = 1; i <= s.length; i++) {
+      //i denotes that word length.
+      for(let j = 0; j<i; j++) {
+            if(dp[i]) break; //will not need to set dp[i] if it's already true
+        if(dp[j] && wordDict.indexOf(s.substring(i,j)) >= 0) {
+  //dp[j] = previous substring, s.substring(i,j) = remaining substring
+          dp[i] = true;
+          break;
+        }
+      }
+    }
+    return Boolean(dp[s.length]);
+  };
 
 console.log(wordBreak("leetcode",["leet", "code"]))
 console.log(wordBreak("catsandog",["cats", "dog", "sand", "and", "cat"]))
