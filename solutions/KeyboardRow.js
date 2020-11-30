6c4121dffeffdab52c668797e57d606a3579ac7e
Granit <ghaxhaj@fordham.edu>
2020-11-30 12:58:21 -0500

    KeyBoardRow

---
+++ solutions/KeyboardRow.js
@@ -0,0 +1,11 @@
+// Given a List of words, return the words that can be typed using letters of
+// alphabetly one row's of American keyboard like the image below.
+
+// Input: ["Hello", "Alaska", "Dad", "Peace"]
+// Output: ["Alaska", "Dad"]
+
+function keyboardRow(arr){
+    let rowOne = ["q",'w','e','r','t','y','u','i','o','p']
+    let rowTwo = ['a','s','d','f','g','h','j','k','l']
+    let rowThree = ['z','x','c','v','b','n','m']
+}
