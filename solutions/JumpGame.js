1c7ab416633ceae34e4b596c79f7222be9516481
Granit <ghaxhaj@fordham.edu>
2020-12-22 20:51:01 -0500

    second

--- solutions/JumpGame.js
+++ solutions/JumpGame.js
@@ -1,7 +1,7 @@
 // Given an array of non-negative integers, you are initially positioned at the first index of the array.
 // Each element in the array represents your maximum jump length at that position.
 // Determine if you are able to reach the last index.
-// Example 1:  
+// Example 1:
 
 // Input: nums = [2,3,1,1,4]
 // Output: true
