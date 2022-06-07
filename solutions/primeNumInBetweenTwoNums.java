b886c3ad9179afadc08d868da27ec78567e051fd
Granit <ghaxhaj@fordham.edu>
2022-06-07 15:55:59 -0400

    primeNumsInBetween

---
+++ solutions/primeNumInBetweenTwoNums.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+class Main {
+
+
+  /*
+Then the third was given two numbers find out how many numbers in between those numbers are divisible by another number.
+*/
+
+  public static int numOfDiv(int a, int b){
+    
+    List<Integer> listOfNums = new ArrayList<Integer>();
+    int result = 0;
+
+    for(int i = a + 1; i < b; i++){
+      listOfNums.add(i);
+    }
+
+
+    for(int i = 0; i < listOfNums.size(); i++){
+      
+      boolean isPrime = true;
+    
+      for(int j = 2; j < listOfNums.get(i); j++){
+        if(listOfNums.get(i) % j == 0){
+          isPrime = false;
+          break;
+        }
+      }
+
+      if(listOfNums.get(i) == 2){
+        isPrime = true;
+      }
+
+       if(isPrime){
+          result++;
+        }
+    }
+    return result;
+  }
+
+  
+  
+  
+  public static void main(String[] args) {
+    System.out.println(numOfDiv(1,10)); //4
+    System.out.println(numOfDiv(1,100)); //25
+  }
+}
